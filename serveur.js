16139a51698a5f48d1358d9e0dbb574458c0ac6c
Hatila <loick69@gmail.com>
2016-03-10 14:36:26 +0100

    intégration css static

--- serveur.js
+++ serveur.js
@@ -2,6 +2,9 @@ var express = require('express');
 
 var app = express();
 
+console.log(__dirname + '/assets');
+app.use(express.static(__dirname + '/assets'));
+
 app.get('/', function(req, res) {
     var message = "Alexandre ne sait pas coder";
     res.render('index.ejs', {param1: message});
