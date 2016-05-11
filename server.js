7b432703e272b86e3619065ff6a905d5221139d0
Hatila <loick69@gmail.com>
2016-05-11 12:24:17 +0200

    Requete préparée

--- server.js
+++ server.js
@@ -62,10 +62,9 @@ app.post('/standard', function(req, res) {
         function (err, result) {
             if (err) {
                 throw err
-            }
-            ;
-            json = result.body; // delivers an array of query results
-            console.log(JSON.stringify(json));
+            };
+            json = JSON.stringify(result.body); // delivers an array of query results
+            console.log(json);
             res.render('interface-standard.ejs', {requete: json});
         });
 });
