cff5a65e8352b21f1cea15851fd80aa86eb8d23b
Hatila <loick69@gmail.com>
2016-06-01 15:20:53 +0200

    fond

--- server.js
+++ server.js
@@ -12,8 +12,7 @@ app.use(express.static(__dirname + '/assets'));
 app.set("view engine", "ejs");
 
 app.get('/', function(req, res) {
-    var message = "Loïck ne sait pas coder";
-    res.render('index.ejs', {param1: message});
+    res.render('index.ejs');
 });
 
 app.get('/test', function(req, res) {
